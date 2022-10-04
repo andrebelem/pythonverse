9ad31162d15e9888b141724c6c3e8496c799425b
Andre Luiz Belem <andrebelem@id.uff.br>
2022-10-04 14:18:40 -0300

    Update sidratools.py

--- sidratools.py
+++ sidratools.py
@@ -49,7 +49,7 @@ def SIDRA2pandas(cod_mun,cod_cultura):
     
     return data2
 
-def plotCULTURA_CIDADE(data,**kwargs):
+def plotCULTURA(data,**kwargs):
     ''' data é um dataframe do pandas com o padrão de colunas ano, área plantada, área colhida, quantidade e rendimento.
     Esta rotina apenas facilita o plot
     '''
